3e16ed09923dcd4d26dc56693324300a6b3a4cd1
Anton <skorodeev@gmail.com>
2023-03-21 11:58:47 +0300

    datascraper dump & restore commands finally mod

--- backend/datascraper/management/commands/dump_data_to_ydsk.py
+++ backend/datascraper/management/commands/dump_data_to_ydsk.py
@@ -6,20 +6,22 @@ from datascraper.logging import init_logger
 from datascraper.models import elapsed_time_decorator
 import subprocess
 
-LOGGER = init_logger('Dump database to Clouds')
+LOGGER = init_logger('Dump data to Yandex Disk')
 
 
 class Command(BaseCommand):
-    help = 'Dump data from database to Cloud services.'
+    help = 'Dump data from database & media folder to Yandex Disk.'
 
     @elapsed_time_decorator(LOGGER)
     def handle(self, *args, **kwargs):
 
-        # making dump file
+        # making dump names
         dt = datetime.now().strftime("%d-%m-%Y_%H-%M-%S")
-        dump_file_name = f"{dt}_dump_db"
+        dump_name = f"{dt}_dump.zip"
+        LOGGER.debug(f'Dump archive name generated: {dump_name}')
+        db_file_name = f"{dt}_db"
 
-        # creating dump file
+        # creating database dump file
         process = subprocess.run(
             ['pg_dump',
                 '--dbname=postgresql://{}:{}@{}:{}/{}'.format(
@@ -31,24 +33,35 @@ class Command(BaseCommand):
                 '-Fc',
                 # '-v',
                 '-f',
-                dump_file_name])
+                db_file_name])
 
         if process.returncode:
             LOGGER.debug('Command failed. Return code : {}'.format(
                 process.returncode))
             exit(1)
 
-        LOGGER.debug("Dump file created")
+        LOGGER.debug("Dump database file created")
 
-        # sending dump to Yandex Disk
+        # zipping database vs media
+        subprocess.run(['zip', '-r', '-q',
+                        f'{dump_name}',
+                        f'{db_file_name}',
+                        'media',
+                        ])
+        LOGGER.debug("Database dump file & media folder zipped to archive")
+
+        # uploading dump to Yandex Disk
+        LOGGER.debug("Starting upload archive to Yandex Disk")
         try:
             yandex = yadisk.YaDisk(token=os.environ.get("YANDEX_TOKEN"))
-            yandex.upload(f'{dump_file_name}',
-                          f'agweather_dump_db/{dump_file_name}',
-                          timeout=(100, 100))
+            yandex.upload(f'{dump_name}',
+                          f'agweather_dump_db/{dump_name}',
+                          timeout=(100, 100),
+                          overwrite=True)
         except Exception as e:
             LOGGER.error(e)
-        LOGGER.debug("Sent to Yandex Disk")
+        LOGGER.debug("Successfully uploaded to Yandex Disk")
 
         # removing temp files
-        os.remove(dump_file_name)
+        os.remove(db_file_name)
+        os.remove(f'{dump_name}')
--- backend/datascraper/management/commands/restore_data_from_ydsk.py
+++ backend/datascraper/management/commands/restore_data_from_ydsk.py
@@ -5,23 +5,29 @@ from datascraper.logging import init_logger
 from datascraper.models import elapsed_time_decorator
 import subprocess
 
-LOGGER = init_logger('Recover database from Yandex Disk')
+LOGGER = init_logger('Recover data from Yandex Disk')
 
 
 class Command(BaseCommand):
-    help = 'Recover database from Yandex Disk'
+    help = 'Recover database & media folder from Yandex Disk'
 
     @elapsed_time_decorator(LOGGER)
     def handle(self, *args, **kwargs):
 
-        # loading dump file from Yandex Disk
+        # loading dump from Yandex Disk
         yandex = yadisk.YaDisk(token=os.environ.get("YANDEX_TOKEN"))
-        last_dump_file = next(yandex.get_last_uploaded())
-        last_dump_file_name = last_dump_file.name
-        LOGGER.debug(f'Last dump file detected: {last_dump_file_name}')
-        last_dump_file.download(last_dump_file_name)
+        last_dump = next(yandex.get_last_uploaded())
+        last_dump_name = last_dump.name
+        LOGGER.debug(f'Last dump archive detected: {last_dump_name}')
+        last_dump.download(last_dump_name)
+
+        # unzipping dump
+        LOGGER.debug('Start unzipping archive')
+        subprocess.run(['unzip', '-uq', f'{last_dump_name}'])
 
         # restoring data from dump file
+        LOGGER.debug("Start recovering database")
+        db_file_name = last_dump_name.replace('_dump.zip', '_db')
         process = subprocess.run(
             ['pg_restore',
                 '--no-owner',
@@ -32,9 +38,9 @@ class Command(BaseCommand):
                     os.environ.get("POSTGRES_PORT"),
                     os.environ.get("POSTGRES_DB")),
                 '-c',
-                last_dump_file_name])
+                db_file_name])
 
-        os.remove(last_dump_file_name)
+        # os.remove(last_dump_file_name)
 
         if process.returncode:
             LOGGER.debug('Command failed. Return code : {}'.format(
@@ -43,3 +49,7 @@ class Command(BaseCommand):
 
         else:
             LOGGER.debug("Database successfully recovered from Yandex Disk.")
+
+        # removing temp files
+        os.remove(db_file_name)
+        os.remove(last_dump_name)
