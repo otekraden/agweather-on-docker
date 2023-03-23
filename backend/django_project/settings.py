e96f1e9f839fecc68da52061aa7dcb7a348a57ef
Anton <skorodeev@gmail.com>
2023-03-23 15:53:11 +0300

    settings mod

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -138,10 +138,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-# ]
+
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
+else:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 # Default primary key field type
